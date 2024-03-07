d38741fdd929bb7c07c5b05c2f1680d801f19441
brilu-22 <231214@virtualwindow.co.za>
2024-03-07 18:41:07 +0200

    changes

--- seeapp/src/App.js
+++ seeapp/src/App.js
@@ -1,22 +1,18 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import NextPage from "./pages/next/NextPage";
 
 function App() {
-  const myRef = useRef(null);
-
   return (
     <section className="hero">
-      <div className="app" ref={myRef}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Chart" element={<NextPage />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Chart" element={<NextPage />} />
+        </Routes>
+      </Router>
     </section>
   );
 }
